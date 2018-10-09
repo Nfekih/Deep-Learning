e4c6e9762e9a2e01aa1f6b61c6c96e18691b8cc1
Nfekih <36096342+Nfekih@users.noreply.github.com>
2018-10-09 11:57:18 +0100

    Update data_augmentation.py

--- data_augmentation.py
+++ data_augmentation.py
@@ -1,13 +1,22 @@
+import  glob
 from keras.preprocessing.image import ImageDataGenerator
 
-
 dataGeneration = ImageDataGenerator(rotation_range=5, horizontal_flip=True, 
-				     width_shift_range=0.2, height_shift_range=0.2)
-files = {"file1": 193, "file2": 400, "file3": 236}
-for file in files.keys():
-    i = 0
-    for img in dataGeneration.flow_from_directory("./initial_data/"+brand, batch_size = 1,
-        save_to_dir="./augmented_data/"+file, save_prefix="image", save_format='jpg'):
-        i+=1
-        if i >= files[file]:
-            break
+				width_shift_range=0.2, height_shift_range=0.2)
+
+brands = ["new balance" , "new converse", "new reebok",
+		 "nike", "old converse", "old reebok" ]
+
+totalIMG=1230 # total images for train and validation
+
+for brand in brands:
+	i=0
+	images = glob.glob("./initialData/"+brand+"/"+brand+"/*.*")
+	for img in dataGeneration.flow_from_directory("./initialData/"+brand,
+							batch_size = 1, 
+							save_to_dir="./augmentedData/"+brand,
+							save_prefix="augIMG", 
+							save_format='jpg'):
+		i+=1
+		if i+len(images) >= totalIMG:
+	            break
